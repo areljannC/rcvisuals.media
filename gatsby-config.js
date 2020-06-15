105dc39db7a1bd234ae78a1fecb3ccd4c9cfed79
AJ Clemente <areljann.c@gmail.com>
2020-06-14 23:59:12 -0400

    CONFIG: Setup 'theme-ui'

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,14 +8,15 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-theme-ui`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        // background_color: `#663399`,
+        // theme_color: `#663399`,
         display: `minimal-ui`,
       },
     }
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,15 @@
+import { base } from '@theme-ui/presets'
+
+const theme = {
+  ...base,
+  colors: {
+    ...base.colors,
+    text: '#040404',
+    background: '#FFFFFF',
+    primary: '#FF5D1A',
+    secondary: '#1B66A5',
+    muted: '#FFA47E',
+  },
+}
+
+export default theme
